63d9f4104b4dd9897506e4e0f6c2a2a428d6a15f
sheldak <samuel.heldak@gmail.com>
2021-04-01 22:14:59 +0200

    add Task model

--- progressTracker/tracker/models.py
+++ progressTracker/tracker/models.py
@@ -8,6 +8,23 @@ class Mock(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
-# class Course(models.Model):
-#     name = models.CharField(max_length=100)
-#     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    class AggregationMethod(models.TextChoices):
+        AVERAGE = 'AVG'
+        WEIGHTED_AVERAGE = 'WAVG'
+
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade_min = models.SmallIntegerField()
+    grade_max = models.SmallIntegerField()
+    is_extra = models.BooleanField()
+    parent_task_id = models.IntegerField()
+    weight = models.FloatField()
+    deadline = models.DateTimeField()
+    aggregation_method = models.CharField(
+        max_length=4, choices=AggregationMethod.choices, default=AggregationMethod.AVERAGE)
--- progressTracker/tracker/serializers.py
+++ progressTracker/tracker/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Mock
+from .models import Mock, Task
+
 
 class MockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mock
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
         fields = '__all__'
