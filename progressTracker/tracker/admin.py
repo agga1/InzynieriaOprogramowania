65d856b7e4ebf2998718db60d8abbb041041f7df
agga1 <agud100@gmail.com>
2021-04-19 13:33:29 +0200

    [CHANGE] is_student clickable in admin close #20

--- progressTracker/tracker/admin.py
+++ progressTracker/tracker/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 # Register your models here.
 from accounts.models import DefaultUser, Student, Teacher
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 
+class DefaultUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = DefaultUser
 
-class CustomAdmin(UserAdmin):
-    pass
+class DefaultUserAdmin(UserAdmin):
+    form = DefaultUserChangeForm
 
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('is_student',)}),
+    )
 
-admin.site.register(DefaultUser, CustomAdmin)
+admin.site.register(DefaultUser, DefaultUserAdmin)
 admin.site.register(Teacher)
 admin.site.register(Student)
