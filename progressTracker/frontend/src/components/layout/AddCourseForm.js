314da6fce2e7237f127abc492ab3ccf41c26da1a
Natalia <natib13@gmail.com>
2021-04-19 18:41:14 +0200

    add course in progress

--- progressTracker/frontend/src/components/layout/AddCourseForm.js
+++ progressTracker/frontend/src/components/layout/AddCourseForm.js
@@ -5,22 +5,40 @@ import Button from './Button'
 
 
 export class AddCourseForm extends Component {
+    
+
     render() {
         return(
-            <Form className="form" >
-                <Form.Group controlId="formBasicName">
+            <Form className="form">
+                <Form.Group controlId="formName">
                     <Form.Label className="form_text">Course name</Form.Label>
                     <Form.Control type="text" placeholder="Enter course name" className="input_window"  onChange={this.props.handleName} value={this.props.name || ''} />
                 </Form.Group>
 
-                <Form.Group controlId="formBasicPassThreshold">
+                <Form.Group controlId="formTeacher">
+                    <Form.Label className="form_text">Teacher</Form.Label>
+                    <Form.Control type="text" placeholder={this.props.teacher} readOnly />
+                </Form.Group>
+
+                <Form.Group controlId="formPassThreshold">
                     <Form.Label className="form_text">Pass threshold</Form.Label>
                     <Form.Control type="text" placeholder="Enter pass threshold" className="input_window"  onChange={this.props.handlePassThreshold} value={this.props.pass_threshold || ''} />
                 </Form.Group>
 
-                <Button type="submit" path={this.props.url} text="Add course">
+                <Form.Group controlId="formStudents">
+                    <Form.Label className="students">Enroll students</Form.Label>
+                    <Form.Control as="select" onChange={this.props.handleStudents} value={this.props.students || ''} as="select" multiple>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Button type="submit"  onClick={this.props.handleSubmit} text="Add course">
                 </Button>
-        </Form>
+            </Form>
         )
         
     }
--- progressTracker/frontend/src/components/views/AddCourse.js
+++ progressTracker/frontend/src/components/views/AddCourse.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react'
-import { Container, Row, Col } from 'reactstrap';
+import React, {  Component, Fragment } from 'react'
+import { Container, Row, Col, Alert } from 'reactstrap';
 import AddCourseForm from '../layout/AddCourseForm'
 import Header from '../layout/Header';
 
@@ -9,11 +9,72 @@ export class AddCourse extends Component {
         
         this.state = {
             name : '',
-            pass_threshold : ''
+            teacher : '',
+            teacher_id : '',
+            pass_threshold : '',
+            students : []
        }
 
         this.handleName = this.handleName.bind(this);
         this.handlePassThreshold = this.handlePassThreshold.bind(this);
+        this.handleStudents = this.handleStudents.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount(){
+        if(localStorage.getItem('token')){
+            fetch('/api/auth/user', {
+                method : 'GET',
+                headers : {
+                    Authorization : `Token ${localStorage.getItem('token')}`
+                }
+            })
+            .then(res => res.json())
+            .then(resp => {
+             if(!resp.is_student){
+                 this.setState({ 
+                     teacher : resp.username,
+                     teacher_id :resp.id
+                 })
+                 
+             }	
+             else{
+                alert('Only teacher can add courses');
+                window.location.href="/student/courses";
+            }
+            })
+            .catch(err => console.log(err));
+        }
+        else{
+            alert('Log into to see the view');
+            window.location.href="/";
+        }
+
+    }
+
+    getStudents(){
+        fetch('/api/students', {
+            method : 'GET',
+            headers : {
+                Authorization : `Token ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(resp => {
+         if(!resp.is_student){
+             this.setState({ 
+                 teacher : resp.username,
+                 teacher_id :resp.id
+             })
+             
+         }	
+         else{
+            alert('Only teacher can add courses');
+            window.location.href="/student/courses";
+        }
+        })
+        .catch(err => console.log(err));
+
     }
 
     handleName = event => {
@@ -27,11 +88,30 @@ export class AddCourse extends Component {
             pass_threshold : e.target.value
         })
     }
+
+    handleStudents = (e) => {
+        this.setState({
+            students : e.target.value
+        })
+    }
+
+    handleSubmit = (e) =>{
+       e.preventDefault();
+       ReactDOM.render(
+        <Alert color="success">
+            Added new course: {this.state.name}\n 
+            pass threshold: {this.state.pass_threshold}\n
+            students: {this.state.students}
+        </Alert>, document.getElementById('root')
+       );
+       alert('added new course: '+this.state.name+ ' '+ this.state.students);
+
+    }
    
 
 
     render() {
-        const { name , pass_threshold} = this.state;
+        const { name, pass_threshold, students} = this.state;
         return (
             <Fragment>
                 <Header button1_text="My Courses" button2_text="Log Out" button1_path="/student/courses" button2_path="/" button2_handle={this.handleLogout}/>
@@ -47,8 +127,12 @@ export class AddCourse extends Component {
                         <AddCourseForm
                             handleName = {this.handleName}
                             handlePassThreshold = {this.handlePassThreshold}
+                            handleStudents = {this.handleStudents}
+                            handleSubmit = {this.handleSubmit}
                             name = {name}
+                            teacher = "super teacher"
                             pass_threshold = {pass_threshold}
+                            students = {students}
                         />
                         </Col>
                     </Row> 
