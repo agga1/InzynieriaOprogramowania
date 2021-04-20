a36d73e17095e4dfb4cf126a71cce68a17c1f0ea
Natalia <natib13@gmail.com>
2021-04-20 22:24:11 +0200

    subtasks visible

--- progressTracker/frontend/src/components/layout/TaskIcon.js
+++ progressTracker/frontend/src/components/layout/TaskIcon.js
@@ -1,12 +1,99 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
 import { Card, CardBody, Row, CardTitle, Col, List, ListInlineItem } from 'reactstrap';
 import Button from './Button'
 
 export class TaskIcon extends Component { 
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          style: '',  
+          show: false,
+          children: []
+        };
+        // this.show = this.show.bind(this);
+        this.onClick = this.onClick.bind(this);
+        this.showChildren = this.showChildren.bind(this);
+      }
+
+
+    onClick=() =>{
+        this.setState((state) => ({
+            show: !state.show,
+            style: "clicked"
+        }))
+        console.log(this.state.show);
+        if(this.state.show==true){
+            this.getChildren();
+        }
+    }
+
+    showChildren=() =>{
+        console.log(this.state.children.length==0)
+        if(this.state.show && this.state.children.length!=0){
+            return(
+                <Row className="p-2">
+                    <Col xs={1}></Col>
+                    <Col xs={11}>
+                    {this.state.children.map((task) =>{
+                        console.log(task);
+                        return (
+                            <Col
+                            xs={12}
+                            key={task.name + task.deadline}
+                            className="mb-4"
+                            >
+                            <TaskIcon
+                                task_name={task.name}
+                                deadline={task.deadline}
+                                // url={task.url}
+                                // onClick = {this.onClick()}
+                                max_points="/50"
+                            />
+                            </Col>
+                        );
+
+                    })}
+                    </Col>
+                </Row>
+            );
+        }else{
+            return(<div></div>)
+        }
+    }
+
+    getChildren(){
+        fetch(this.props.url + "children", {
+            method: "GET",
+            headers: {
+              Authorization: `Token ${localStorage.getItem("token")}`,
+            },
+          })
+            .then((response) => {
+              if (response.status > 400) {
+                return this.setState(() => {
+                  return { placeholder: "Something went wrong!" };
+                });
+              }
+              return response.json();
+            })
+            .then((data) => {
+              this.setState(() => {
+                return {
+                  children: data.children,
+                };
+              });
+            });
+    }
+
+
+    
+    
     render() {
         return (
-            <div>
-                <Card className="icon">
+            <Fragment>
+            <a className="stretched-link" onClick={this.onClick}>
+                <Card className={`icon ${ this.state.style }`} >
                     <Row >
                         <Col xs={11}>
                         <CardBody className="pb-0">
@@ -32,12 +119,16 @@ export class TaskIcon extends Component {
                             <List >
                                 <ListInlineItem href="#" className="task-link pr-3">+Task</ListInlineItem>
                                 <ListInlineItem href="#"  className="task-link pr-3 ">Rate</ListInlineItem>
-                                <ListInlineItem href="{this.props.task_details_path}"  className="task-link">Details</ListInlineItem>
+                                <ListInlineItem href="#"  className="task-link">Details</ListInlineItem>
                             </List>
                         </Col>
                     </Row>
                 </Card>
+            </a>
+            <div>
+            {this.showChildren()}
             </div>
+        </Fragment>
         )
     }
 }
