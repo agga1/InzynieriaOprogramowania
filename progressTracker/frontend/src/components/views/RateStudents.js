cf8f8c5bb464c249ba52b9423334607a8f645fc6
Natalia <natib13@gmail.com>
2021-04-27 23:46:44 +0200

    adding float grade is available

--- progressTracker/frontend/src/components/views/RateStudents.js
+++ progressTracker/frontend/src/components/views/RateStudents.js
@@ -144,7 +144,8 @@ export class RateStudents extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        if(this.state.rate < this.state.task.grade_max && this.state.rate > this.state.task.grade_min){
+        var new_grade = parseFloat(this.state.rate.replace(",","."));
+        if(new_grade < this.state.task.grade_max && new_grade > this.state.task.grade_min){
             fetch(localStorage.getItem('taskUrl')+'add_grade/', {
                 method : 'POST',
                 headers : {
@@ -153,7 +154,7 @@ export class RateStudents extends Component {
                 },
                 body : JSON.stringify({
                     students: [this.state.student_id],
-                    grades: [this.state.rate]
+                    grades: [new_grade]
                 })
             })
             .then(res => res.json())
@@ -161,6 +162,9 @@ export class RateStudents extends Component {
                 this.handleCancel()
             )
             .catch(err => console.log(err));
+        }
+        else{
+            alert("Enter proper grade from range: ["+this.state.task.grade_min+", "+this.state.task.grade_max+"]!");
         }
     }
 
@@ -189,65 +193,63 @@ export class RateStudents extends Component {
           );
         } else {
           return (
-            <Col xs={10} className="pr-4">
-                <Table striped className="students-list">
-                    <thead>
-                        <tr>
-                        <th className="td-sm">no.</th>
-                        <th colSpan="3">Name</th>
-                        <th className="td-sm">Points</th>
-                        <th className="td-sm">Rate</th>
-                        <th className="td-sm">Orderxd</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.students.map(student=> {
-                            return (
-                            <tr key={student.user.id}>
-                                <td className="td-sm" scope="row">{this.state.students.indexOf(student)}</td>
-                                <td colSpan="3">{student.user.first_name} {student.user.last_name}</td>
-                                <td className="td-sm">{this.getGrade(student)}</td>
-                                <td className="td-sm"><a className='btn' role="button" aria-pressed="false" onClick={() => this.showModal(student)}>Rate</a></td>
-                                <td className="td-sm"><a className='btn btn-sm' role="button" aria-pressed="true" >b</a></td>
-                            </tr>  
-                            );
-                        })} 
-                    </tbody>
-                </Table>
-        </Col>
+            <Container fluid>
+                <Modal
+                    title={this.state.title}
+                    show={this.state.show}
+                    handleSubmit = {this.handleSubmit}
+                    handleCancel = {this.handleCancel}
+                    handleChange = {this.handleChange}
+                    rate = {this.state.rate}
+                    max_grade = {this.state.task.grade_max}
+                />
+                <Row className="mt-4 mb-5 ml-3">
+                    <Col xs={3}/>
+                    <Col xs={6} className="heading login_heading text-left">{this.state.task.name}</Col>                             
+                </Row>
+
+                <Row>
+                    <Col xs={2} className="ml-0 pl-0">
+                        <Sidebar/>
+                    </Col>
+                    <Col xs={10} className="pr-4">
+                        <Table striped className="students-list">
+                            <thead>
+                                <tr>
+                                <th className="td-sm">no.</th>
+                                <th colSpan="3">Name</th>
+                                <th className="td-sm">Points</th>
+                                <th className="td-sm">Rate</th>
+                                <th className="td-sm">Orderxd</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.students.map(student=> {
+                                    return (
+                                    <tr key={student.user.id}>
+                                        <td className="td-sm" scope="row">{this.state.students.indexOf(student)}</td>
+                                        <td colSpan="3">{student.user.first_name} {student.user.last_name}</td>
+                                        <td className="td-sm">{this.getGrade(student)}</td>
+                                        <td className="td-sm"><a className='btn' role="button" aria-pressed="false" onClick={() => this.showModal(student)}>Rate</a></td>
+                                        <td className="td-sm"><a className='btn btn-sm' role="button" aria-pressed="true" >b</a></td>
+                                    </tr>  
+                                    );
+                                })} 
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+        </Container>
           );
         }
       }
 
 
     render() {
-        const {rate} = this.state;
         return (
             <Fragment>
                 <Header button1_text="My Courses" button2_text="Log Out" button1_path="/student/courses" button2_path="/" is_logout={true}/>
-                <Container fluid>
-                    <Modal
-                        title={this.state.title}
-                        show={this.state.show}
-                        handleSubmit = {this.handleSubmit}
-                        handleCancel = {this.handleCancel}
-                        handleChange = {this.handleChange}
-                        rate = {rate}
-                        max_grade = {this.state.task.grade_max}
-                    />
-                    <Row className="mt-4 mb-5 ml-3">
-                        <Col xs={3}/>
-                        <Col xs={6} className="heading login_heading text-left">{this.state.task.name}</Col>                             
-                    </Row>
-
-                    <Row>
-                        <Col xs={2} className="ml-0 pl-0">
-                            <Sidebar/>
-                        </Col>
-                        {this.prepareView()}                        
-                    </Row>
-                    
-                </Container>    
+                    {this.prepareView()}                         
                 <Footer/>               
             </Fragment>
         )
