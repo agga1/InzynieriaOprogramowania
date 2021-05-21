e44a0cd19e26e8ff939bc91b42c6e323ddc08f4f
agga1 <agud100@gmail.com>
2021-05-21 15:22:26 +0200

    ready for heroku

--- progress_tracker_project/asgi.py
+++ progress_tracker_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for progressTracker project.
+ASGI config for progress_tracker_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progressTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progress_tracker_project.settings')
 
 application = get_asgi_application()
--- progress_tracker_project/wsgi.py
+++ progress_tracker_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for progressTracker project.
+WSGI config for progress_tracker_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progressTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'progress_tracker_project.settings')
 
 application = get_wsgi_application()
